8cbf5e3dcb1f162bee4847cd040421f070f2f869
Camila Rivera <carivera14@>
2022-06-22 00:19:49 -0500

    .h de metodos externos

---
+++ MetodosExternos.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+#pragma once
+   
+class MetodosExternos{
+    private:
+        string nombreArchivo; 
+    public:
+
+    MetodosExternos(string);
+    ~MetodosExternos();
+    void ordenarPorDirecta();
+    void ordenarPorNatural();
+};
