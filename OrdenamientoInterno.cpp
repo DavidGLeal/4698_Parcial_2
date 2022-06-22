0be0f1f7485113f0ab49b22c8547d6ef339f935c
SebastianTorres12 <105263296+SebastianTorres12@users.noreply.github.com>
2022-06-21 20:26:34 -0500

    Creación OrdenamientoInterno.cpp
    Grupo 10

---
+++ OrdenamientoInterno.cpp
@@ -0,0 +1,25 @@
+#include "OrdenamientoInterno.h"
+
+void OrdenamientoInterno::ordenarIntercambio(){
+	
+}
+
+void OrdenamientoInterno::ordenarBurbuja(){
+	
+}
+
+void OrdenamientoInterno::ordenarQuicksort(){
+	
+}
+
+void OrdenamientoInterno::ordenarShellSort(){
+	
+}
+
+void OrdenamientoInterno::ordenarDistribucion(){
+	
+}
+
+void OrdenamientoInterno::ordenarRadix(){
+	
+}
