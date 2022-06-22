95beb9ecb04f6a5c03084fa288fd54a135707d4c
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-22 18:58:32 -0500

    Implementacion ordenacion por intercambio con clase vector de enteros

--- OrdenamientoInterno.cpp
+++ OrdenamientoInterno.cpp
@@ -1,7 +1,15 @@
 #include "OrdenamientoInterno.h"
 
-void OrdenamientoInterno::ordenarIntercambio(){
-	
+void OrdenamientoInterno::ordenarIntercambio(Vector<int> &arreglo){
+    for (int i{}; i < arreglo.size(); i++) {
+        for (int j = 0; j < arreglo.size() - 1; j++) {
+            if (arreglo[j] < arreglo[j + 1]) {
+                int aux = arreglo[j];
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = aux;
+            }
+        }
+    }
 }
 
 void OrdenamientoInterno::ordenarBurbuja(){
