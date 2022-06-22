6fd2e7e87c126de66836ce8e1e4bc164bc45d26d
SebastianTorres12 <105263296+SebastianTorres12@users.noreply.github.com>
2022-06-21 20:25:39 -0500

    Creación de interfaz OrdenamientoInterno.h
    Grupo 10

---
+++ OrdenamientoInterno.h
@@ -0,0 +1,16 @@
+#pragma onde
+#include <iostream>
+using namespace std;
+
+class OrdenamientoInterno{
+	
+	public:
+		
+		virtual void ordenarIntercambio();
+		virtual void ordenarBurbuja();
+		virtual void ordenarQuicksort();
+		virtual void ordenarShellSort();
+		virtual void ordenarDistribucion();
+		virtual void ordenarRadix();
+		
+}
