67d84aa0ee13dbbfb7715f62924ed66d6d13baa8
Camila Rivera <carivera14@>
2022-06-23 17:00:13 -0500

    Agrego algunos comentarios'

--- MetodosExternos.h
+++ MetodosExternos.h
@@ -8,21 +8,24 @@ class MetodoExterno: public ImprimirExterno{
     private:
         string nombreArchivo{"ordenada.txt"}; //Por default
 
+        //!METODOS DE AMBAS FUNCIONES
+
+        void abrir(fstream *f, string nom, int tip);
+        void cerrar(fstream *f);
+        void limpiar();
+
         //!ORDENACION NATURAL. METODOS INTERNOS
 
 
         void generarAleatorios(int min, int max);
-    
-        void abrir(fstream *f, string nom, int tip);
         void insertar(int d, string nom);
-        void cerrar(fstream *f);
-        void limpiar();
-
         void particionInicial();
         void particionFusion();
         void intercalacionDeArchivo(string nom1, string nom2, string nom3, string nom4);
 
         //!ORDENACION DIRECTA. METODOS INTERNOS
+
+
 
     public:
         MetodoExterno(string);
