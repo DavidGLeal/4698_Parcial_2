c7918248d1b24915d588528a297a53e09618e735
Daniel_Armas <105186962+Ale8Daniel8Armas@users.noreply.github.com>
2022-06-26 13:43:56 -0500

    estructura o cuerpo

---
+++ Hash/menu.h
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h> 
+#include <windows.h>
+#include <stdlib.h>
+#include "ingresoDatos.h"
+#include "windows.h"
+
+#define TECLA_ARRIBA 72
+#define TECLA_ABAJO 80
+#define TECLA_ENTER 13
+int validar_arreglo(char arreglo[]);
+using namespace std;
+int menu(const char *titulo, const char *opciones[], int n);
+
+
+int ingresoD(){
+
+	IngresoDatos *i = new IngresoDatos();
+	cout << "\n";
+	int num = i->funcionPrincipalEnteros("Ingrese sus numeros: ");
+
+	return num;
+}
+
+
+int* ingresar(){
+
+	int *arreglo = new int[10];
+	
+	int cont{0};
+
+	cout<<"\n";
+	//cout << "contador " <<cont <<endl;
+
+	
+	for (int i = 0; i< 10; i++){
+
+		int num{ingresoD()};
+
+		if(num == -10101010){
+			i = 10;
+		}else{
+			arreglo[i] = num;
+			++cont;
+		}
+	}
+
+	for (int i = 0; i< cont; i++){
+		cout<< arreglo[i] << " ";
+	}
+
+	cout<<"\n";
+	system("pause");
+	
+	return arreglo;
+}
+
+
+
+int IngresarDatos(){
+	//char arreglo[10]={};
+	char *arreglo=new char[10];
+	int N{0};
+	int arreglovalido{0};
+
+	do
+   	{
+       printf("INGRESA UN arreglo ENTERO: ");
+       scanf("%s",arreglo);
+       N=validar_arreglo(arreglo);
+
+   	}while(N==0);
+
+   	arreglovalido=atoi(arreglo);
+   	printf("\nEL arreglo ES: %i",arreglovalido);
+
+   	getch();
+   	return 1;
+}
+
+int validar_arreglo(char* arreglo)
+{
+    int i;
+    for(i=0; i<strlen(arreglo); i++)
+    {
+        if(!(isdigit(arreglo[i])))
+        {
+            printf("\nINGRESA SOLO arregloS\n");
+            getch();
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+void MenuIngreso(){
+   bool repite = true;
+   int opcion;   
+  
+   const char *titulo = "Escoja un ingreso";
+   const char *opciones[] = {"Ingreso con numeros", "Ingreso con texto","Retornar"};
+   int n = 3;  
+   do 
+   {
+      opcion = menu(titulo, opciones, n);
+		
+		switch(opcion)
+		{
+			case 1:{
+				system("cls");
+				int *arreglo = ingresar();
+				break;
+			}		
+			case 2:{
+				system("cls");
+				int *arreglo = ingresar();
+				break;
+			}					
+			case 3:{
+				system("cls");
+				cout<<endl;
+				cout<<"Regresando..."<<endl;
+				repite=false;
+				break;
+			}											
+		}
+	}while(repite);
+}
+
+void MenuPrincipal(){
+
+	system("mode con: cols=50 lines=23");
+	bool repite = true;
+	int opcion;   
+ 
+   
+   const char *titulo = "HASH CERRADO";
+   const char *opciones[] = {"Sondeo Lineal", "Sondeo Cuadratico","Doble Hash","Salir del programa"};
+   int n =4;  
+ 
+   do {
+      opcion = menu(titulo, opciones, n);
+		
+		switch(opcion){
+			case 1:{
+				system("cls");
+				MenuIngreso();
+				break;
+			}
+			case 2:{
+				system("cls");
+				MenuIngreso();
+				break;
+			}		
+			case 3:{
+				system("cls");
+				MenuIngreso();
+				break;
+			}		
+			case 4:{
+				cout<<endl;
+				printf("\n\n\n");
+				cout<<"Saliendo del programa..."<<endl;
+				repite=false;
+				break;
+			}										
+	}
+	printf("\n\n\n");
+	system("pause");
+	}while(repite);
+}
+
+int menu(const char titulo[], const char *opciones[], int n){
+   int opcionSeleccionada = 1;  
+ 
+   int tecla;
+ 
+   bool repite = true; 
+ 
+   do {
+      system("cls");
+      //system("Color 8B");
+      gotoxy(5, 3 + opcionSeleccionada); cout << "->" << endl;
+ 
+      
+      gotoxy(15, 2); cout << titulo;
+ 
+      
+      for (int i = 0; i < n; ++i) {
+         gotoxy(10, 4 + i); cout <<"["<< i + 1 << "] | " << opciones[i];
+      }
+ 
+      
+ 
+      do {
+         tecla = getch();
+         
+      } while (tecla != TECLA_ARRIBA && tecla != TECLA_ABAJO && tecla != TECLA_ENTER);
+ 
+      switch (tecla) {
+ 
+         case TECLA_ARRIBA:   
+ 
+            opcionSeleccionada--;
+ 
+            if (opcionSeleccionada < 1) {
+               opcionSeleccionada = n;
+            }
+ 
+            break;
+ 
+         case TECLA_ABAJO:
+            opcionSeleccionada++;
+ 
+            if (opcionSeleccionada > n) {
+               opcionSeleccionada = 1;
+            }
+ 
+            break;
+ 
+         case TECLA_ENTER:
+            repite = false;
+            break;
+      }
+ 
+   } while (repite);
+ 
+   return opcionSeleccionada;
+}
---
+++ Hash/menuPrincipal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "menu.h"
+
+int main(int argc, char** argv) {
+	
+	MenuPrincipal();
+	
+	return 0;
+}   
