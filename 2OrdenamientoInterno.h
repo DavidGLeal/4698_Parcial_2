08108dc94fe49eea4df0d31d0f0a92015a179105
Sebastian Torres <sptorres>
2022-06-26 15:34:07 -0500

    Grupo 10
    Borrado virtual

--- 2OrdenamientoInterno.h
+++ 2OrdenamientoInterno.h
@@ -9,14 +9,14 @@ class OrdenamientoInterno{
 		Vector vector;
 	public:
 		
-		virtual Vector &ordenarIntercambio();
-		virtual void ordenarBurbuja();
-		virtual void ordenarQuicksort(Vector &v, int inicio, int fin);
-		virtual void ordenarShellSort();
-		virtual void ordenarDistribucion();
-		virtual void ordenarBucket();
-		virtual void ordenarRadix();
-		virtual void imprimirInterno(); 	
+		Vector &ordenarIntercambio();
+		void ordenarBurbuja();
+		void ordenarQuicksort(Vector &v, int inicio, int fin);
+		void ordenarShellSort();
+		void ordenarDistribucion();
+		void ordenarBucket();
+		void ordenarRadix();
+		void imprimirInterno(); 	
 		Vector getVector();
 		void setVector(Vector vector);
 };
