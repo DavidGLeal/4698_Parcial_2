d141e401c660382a57bb890dcd43e20aef85a77d
Camila Rivera <carivera14@>
2022-06-27 02:21:18 -0500

    Arreglo funciones

--- mainPruebaVector.cpp
+++ mainPruebaVector.cpp
@@ -19,51 +19,9 @@ int main(){
     v.push_back(74);
     v.push_back(1);
 
-<<<<<<< HEAD
-//<<<<<<< HEAD
-=======
->>>>>>> d9edc777ee1b893840e91e301e17803e15e20de1
-	/*
-    OrdenamientoInterno *intercambio = new OrdenamientoInterno(v);
-    OrdenamientoInterno *burbuja = new OrdenamientoInterno(v);
-	OrdenamientoInterno *shellsort = new OrdenamientoInterno(v);
-	*/
+
 	OrdenamientoInterno *bucket = new OrdenamientoInterno(v);
-	//OrdenamientoInterno *radix = new OrdenamientoInterno(v);
-	
-	//OrdenamientoInterno *distribucion = new OrdenamientoInterno(v);
-	/*
-    intercambio->imprimirInterno();
-    intercambio->ordenarIntercambio();
-    intercambio->imprimirInterno();
-	
-    burbuja->imprimirInterno();
-    burbuja->ordenarBurbuja();
-	burbuja->imprimirInterno(); 
-	
-	shellsort->imprimirInterno();
-    shellsort->ordenarShellSort(); 
-	shellsort->imprimirInterno(); 
-	
-	distribucion->imprimirInterno();
-    distribucion->ordenarDistribucion();
-	distribucion->imprimirInterno(); 
-	
-	bucket->imprimirInterno();
-    bucket->ordenarBucket();
-	bucket->imprimirInterno(); 
 
-    OrdenamientoInterno *a = new OrdenamientoInterno(v);
-
-    a->imprimirInterno();
-    a->ordenarRadix();
-    a->imprimirInterno();
->>>>>>> d9edc777ee1b893840e91e301e17803e15e20de1
-	
-    radix->imprimirInterno();
-    radix->ordenarRadix();
-	radix->imprimirInterno();
-    */
 	 
 	bucket->imprimirInterno();
     bucket->ordenarBucket();
