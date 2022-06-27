8a37c414d776d809e97618d0b3073048dd455c38
Mathias Alejandro Guevara Cruz <105067369+maguevara10@users.noreply.github.com>
2022-06-27 08:49:48 -0500

    Radix con negativos

--- 2OrdenamientoInterno.cpp
+++ 2OrdenamientoInterno.cpp
@@ -345,41 +345,111 @@ void OrdenamientoInterno::ordenarBucket(){
  */
 void OrdenamientoInterno::ordenarRadix(){
 
-    auto maximo = [&] (){
-	int mx = *(vector.get(0)); 
-    	for (int i = 1; i < vector.size(); i++) 
-        if (*(vector.get(i)) > mx) 
-        mx = *(vector.get(i)); 
-    	return mx;
-	};
-	
-    int m = maximo(); 
-
-    auto Sort = [&] (int exp){	
-	    Vector salida,contador; 
-	    for (int i = 0; i < 10; i++){	        	
-	        contador.push_back(0);
-	    }
-	    for (int i = 0; i < vector.size(); i++){	        	
-	        salida.push_back(0);
-	    } 		  
-	    for (int i = 0; i < vector.size(); i++){		
-	        *(contador.get((*(vector.get(i)) / exp) % 10))=*(contador.get((*(vector.get(i)) / exp) % 10))+1;
-            }		  
-	    for (int i = 1; i < 10; i++) {		    	
-		*(contador.get(i)) += *(contador.get(i-1)); 
-	    }
-	    for (int i = vector.size()-1; i >= 0; i--) {
-		*(salida.get(*(contador.get((*(vector.get(i))/exp)%10))-1))=*(vector.get(i)) ;				
-		*(contador.get((*(vector.get(i)) / exp) % 10))=*(contador.get((*(vector.get(i)) / exp) % 10))-1;		
-            }   
-	    for (int i = 0; i < vector.size(); i++){
-		*(vector.get(i)) = *(salida.get(i)); 
-	    }	        
-    };
+    int tam{vector.size()};
+    Vector vN,vP;
     
-    for (int exp = 1; m / exp > 0; exp *= 10) 
-        Sort(exp); 
+    for(int i=0; i<tam; i++){
+        if(vector[i]<0)
+        {
+        	vN.push_back(abs(vector[i]));
+		}else{
+			vP.push_back(vector[i]);
+		}
+    }
+    int auxNCount=0;
+    if(vN.size()!=0)
+    {
+    	auto maximoN = [&] (){
+		int mxN = *(vN.get(0)); 
+	    	for (int i = 1; i < vN.size(); i++) 
+	        if (*(vN.get(i)) > mxN) 
+	        mxN = *(vN.get(i)); 
+	    	return mxN;
+		};
+		
+	    int mN = maximoN(); 
+	
+	    auto SortN = [&] (int expN){	
+		    Vector salidaN,contadorN; 
+		    for (int i = 0; i < 10; i++){	        	
+		        contadorN.push_back(0);
+		    }
+		    for (int i = 0; i < vN.size(); i++){	        	
+		        salidaN.push_back(0);
+		    } 		  
+		    for (int i = 0; i < vN.size(); i++){		
+		        *(contadorN.get((*(vN.get(i)) / expN) % 10))=*(contadorN.get((*(vN.get(i)) / expN) % 10))+1;
+	        }		  
+		    for (int i = 1; i < 10; i++) {		    	
+				*(contadorN.get(i)) += *(contadorN.get(i-1)); 
+		    }
+		    for (int i = vN.size()-1; i >= 0; i--) {
+				*(salidaN.get(*(contadorN.get((*(vN.get(i))/expN)%10))-1))=*(vN.get(i)) ;				
+				*(contadorN.get((*(vN.get(i)) / expN) % 10))=*(contadorN.get((*(vN.get(i)) / expN) % 10))-1;		
+	        }   
+		    for (int i = vN.size()-1; i>=0; i--){
+				*(vN.get(i)) = *(salidaN.get(i)); 
+		    }	        
+	    };
+	    
+	    for (int expN = 1; mN / expN > 0; expN *= 10) 
+	        SortN(expN);
+	}
+	
+	
+	if(vP.size()!=0)
+	{
+		auto maximoP = [&] (){
+			int mxP = *(vP.get(0)); 
+	    	for (int i = 1; i < vP.size(); i++) 
+	        if (*(vP.get(i)) > mxP) 
+	        mxP = *(vP.get(i)); 
+	    	return mxP;
+		};
+		
+	    int mP = maximoP(); 
+	
+	    auto SortP = [&] (int expP){	
+		    Vector salidaP,contadorP; 
+		    for (int i = 0; i < 10; i++){	        	
+		        contadorP.push_back(0);
+		    }
+		    for (int i = 0; i < vP.size(); i++){	        	
+		        salidaP.push_back(0);
+		    } 		  
+		    for (int i = 0; i < vP.size(); i++){		
+		        *(contadorP.get((*(vP.get(i)) / expP) % 10))=*(contadorP.get((*(vP.get(i)) / expP) % 10))+1;
+	        }		  
+		    for (int i = 1; i < 10; i++) {		    	
+				*(contadorP.get(i)) += *(contadorP.get(i-1)); 
+		    }
+		    for (int i = vP.size()-1; i >= 0; i--) {
+				*(salidaP.get(*(contadorP.get((*(vP.get(i))/expP)%10))-1))=*(vP.get(i)) ;				
+				*(contadorP.get((*(vP.get(i)) / expP) % 10))=*(contadorP.get((*(vP.get(i)) / expP) % 10))-1;		
+	        }   
+		    for (int i = 0; i < vP.size(); i++){
+				*(vP.get(i)) = *(salidaP.get(i)); 
+		    }	        
+	    };
+	    
+	    for (int expP = 1; mP / expP > 0; expP *= 10) 
+	        SortP(expP);	
+	}
+	if(vN.size()!=0)
+    {
+		for (int i = vN.size()-1; i >=0; i--){
+			*(vector.get(auxNCount)) = *(vN.get(i)); 
+			vector[auxNCount]=-1*(vector[auxNCount]);
+			auxNCount++;
+		}
+	}
+	if(vP.size()!=0)
+    {
+		for (int i = 0; i < vP.size(); i++){
+			*(vector.get(auxNCount)) = *(vP.get(i)); 
+			auxNCount++;
+		}
+	}
 } 
 
 void OrdenamientoInterno::imprimirInterno(){
