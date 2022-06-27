3ebf6d8b07c117cc8dd83ec34922dffd34b85878
Dark230900 <105337395+Dark230900@users.noreply.github.com>
2022-06-26 19:19:16 -0500

    Update 4Busqueda.cpp

--- 4Busqueda.cpp
+++ 4Busqueda.cpp
@@ -1,40 +1,44 @@
 #include "4Busqueda.h"
 
 
-void Busqueda::busquedaBinaria(){
-	int numeros[] = {1,2,3,4,5,6,7,8,9,10};//arreglo a usar
-	int inf,sup,mitad,dato,i;
-	char band='F';//bandera sirve para comprobar si existe o no el elemento 
-	cout<<"El numero a encontrar: ";
-	cin>>dato;//numero a buscar
-	//Algoritmo de la Busqueda Binaria
-	inf=0; //limite inferior del arreglo
-	sup=10; // limite superior del arreglo
-	i=0;
-	while((inf<=sup)&&(i<10)){
-		mitad = (inf+sup)/2;
-		if(numeros[mitad] == dato){
-			band='V';
-			break;
-		}
-		if(numeros[mitad]>dato){
-			sup = mitad;
-			mitad = (inf+sup)/2;
-		}
-		if(numeros[mitad]<dato){
-			inf = mitad;
-			mitad = (inf+sup)/2;
-		}
-		i++;
-	}
-	//Enviar mensaje si se encontro o no el dato.
-	if(band == 'V'){
-		cout<<"El numero se encontro en la pos: "<<mitad<<endl;
-	}
-	else{
-		cout<<"El numero NO se encontro";
-	}
-	
+void busquedaBinaria(){
+    int numeros[10];//arreglo a usar
+    int inf,sup,mitad,dato,i;
+    char band='F';//bandera sirve para comprobar si existe o no el elemento
+    for(int i=0;i<10;i++){
+        cout<<"Ingrese el elemento:"<<endl;
+        cin>>numeros[i];
+    }
+    cout<<"El numero a encontrar: ";
+    cin>>dato;//numero a buscar
+    //Algoritmo de la Busqueda Binaria
+    inf=0; //limite inferior del arreglo
+    sup=10; // limite superior del arreglo
+    i=0;
+    while((inf<=sup)&&(i<10)){
+        mitad = (inf+sup)/2;
+        if(numeros[mitad] == dato){
+            band='V';
+            break;
+        }
+        if(numeros[mitad]>dato){
+            sup = mitad;
+            mitad = (inf+sup)/2;
+        }
+        if(numeros[mitad]<dato){
+            inf = mitad;
+            mitad = (inf+sup)/2;
+        }
+        i++;
+    }
+    //Enviar mensaje si se encontro o no el dato.
+    if(band == 'V'){
+        cout<<"El numero se encontro en la pos: "<<mitad<<endl;
+    }
+    else{
+        cout<<"El numero NO se encontro";
+    }
+
 }
 
 void Busqueda::busquedaSecuencial(){
