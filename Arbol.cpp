8b38923b96cdb3565dfdee34925c8dfd95e9e5d3
Camila Rivera <carivera14@>
2022-07-04 18:38:09 -0500

    Comentarios

--- Arbol.cpp
+++ Arbol.cpp
@@ -1,13 +1,32 @@
 #include "Arbol.h"
 
+/**
+ * Establece el valor de la variable privada arbol al valor del parámetro _arbol.
+ * 
+ * @param _arbol El nodo raíz del árbol.
+ */
 void Arbol::setArbol(Nodo *_arbol)
 {
     arbol = _arbol;
 }
+
+/**
+ * Devuelve la raíz del árbol.
+ * 
+ * @return El nodo raíz del árbol.
+ */
 Nodo *Arbol::getArbol()
 {
     return arbol;
 }
+
+/**
+ * Crea un nuevo nodo con el valor del parámetro.
+ * 
+ * @param dato El valor del nodo
+ * 
+ * @return Un puntero a un objeto Nodo.
+ */
 Nodo *Arbol::crearNodo(int dato)
 {
     Nodo *nuevoNodo = new Nodo();
@@ -16,6 +35,15 @@ Nodo *Arbol::crearNodo(int dato)
     nuevoNodo->setIzquierda(NULL);
     return nuevoNodo;
 }
+
+/**
+ * Si el árbol está vacío, cree un nuevo nodo y asígnelo al árbol. De lo contrario, si los datos son
+ * menores que la raíz, insértelos en el subárbol izquierdo. De lo contrario, insértelo en el subárbol
+ * derecho
+ * 
+ * @param arbol El árbol
+ * @param dato el valor a insertar
+ */
 void Arbol::insertarNodo(Nodo *&arbol, int dato)
 {
     if (arbol == NULL)
@@ -39,7 +67,15 @@ void Arbol::insertarNodo(Nodo *&arbol, int dato)
         }
     }
 }
+
 //RECORRIDO EN PREORDEN
+/**
+ * *|CURSOR_MARCADOR|*
+ * 
+ * @param arbol es la raiz del arbol
+ * 
+ * @return el valor del nodo.
+ */
 void Arbol::preOrden(Nodo *arbol)
 {
     if (arbol == NULL)
