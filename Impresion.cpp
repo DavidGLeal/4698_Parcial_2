64fbed1509456b990ea5b4b1358def5024644656
PaolaMoncayo <87495121+PaolaMoncayo@users.noreply.github.com>
2022-07-20 09:25:43 -0500

    Delete Impresion.cpp

--- Impresion.cpp
+++
@@ -1,7 +0,0 @@
-#include "CProceso.h"
-#include "Impresion.h"
-#include <iostream>
-
-void Impresion::mostrarEnConsola(CProceso obj) {
-    std::cout << obj.getNumerador() << "/" << obj.getDenominador() << std::endl;
-}
