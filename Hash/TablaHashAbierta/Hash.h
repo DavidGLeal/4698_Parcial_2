ecfd0bacb6435561daa4f3e10cfc3fd90538f476
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2022-07-19 23:07:48 -0500

    Update Hash.h

--- Hash/TablaHashAbierta/Hash.h
+++ Hash/TablaHashAbierta/Hash.h
@@ -1,15 +1,15 @@
 /*
 	*Universidad de las Fuerzas Armadas "ESPE"
     *Enunciado del problema:
-	    *Programa que realize hash cerrado de enteros y strings .
+	    *Programa que realize hash abierto.
      *Autores:
         *Curso de Estructura de Datos NRC: 4698
 	*Fecha de creacion: 
-        * 24-06-2022
+        * 15-07-2022
 	*Fecha de modificacion:
-        * 29-06-2022
+        * 19-07-2022
     *GitHub del grupo:	
-        
+        https://github.com/DavidGLeal/4698_Parcial_2/tree/Codigos_proyecto/Hash/TablaHashAbierta
 */
 
 #ifndef HASH_H
