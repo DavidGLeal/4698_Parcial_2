593e4d21a063b3ceac745b106df1cd34337a674c
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2022-07-19 23:08:23 -0500

    Update HashStr.h

--- Hash/TablaHashAbierta/HashStr.h
+++ Hash/TablaHashAbierta/HashStr.h
@@ -1,3 +1,17 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+    *Enunciado del problema:
+	    *Programa que realize hash abierto.
+     *Autores:
+        *Curso de Estructura de Datos NRC: 4698
+	*Fecha de creacion: 
+        * 15-07-2022
+	*Fecha de modificacion:
+        * 19-07-2022
+    *GitHub del grupo:	
+        https://github.com/DavidGLeal/4698_Parcial_2/tree/Codigos_proyecto/Hash/TablaHashAbierta
+*/
+
 #pragma once
 #ifndef HASHSTR_H
 #define HASHSTR_H
