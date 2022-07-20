1819656b0ac33bd0fe03988efd2846b94c280abf
Johan-Romo <87491302+Johan-Romo@users.noreply.github.com>
2022-07-19 22:58:01 -0500

    Update NodoStr.cpp

--- Hash/TablaHashAbierta/NodoStr.cpp
+++ Hash/TablaHashAbierta/NodoStr.cpp
@@ -1,3 +1,10 @@
+/*
+Autores: Johan Romo, Cristhopher Villamarín.
+Fecha de creacion: 16/07/2022
+Fecha de modificacion: 19/07/2022
+Grupo 10
+Github del grupo: jaromo3@espe.edu.ec, cavillamarin9@espe.edu.ec
+*/
 #include "NodoStr.h"
 #include <cstring>
 #include <cstdlib>
