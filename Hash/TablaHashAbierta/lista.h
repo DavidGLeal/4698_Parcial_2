6369de4b898bc9c923c2853410db0565248971bf
Johan-Romo <87491302+Johan-Romo@users.noreply.github.com>
2022-07-19 22:56:28 -0500

    Update lista.h

--- Hash/TablaHashAbierta/lista.h
+++ Hash/TablaHashAbierta/lista.h
@@ -4,7 +4,7 @@ Fecha de creacion: 16/07/2022
 Fecha de modificacion: 19/07/2022
 Grupo 10
 Github del grupo: jaromo3@espe.edu.ec, cavillamarin9@espe.edu.ec
-*
+*/
 
 #ifndef LISTA_H
 #define LISTA_H
