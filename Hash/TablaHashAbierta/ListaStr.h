b9d035e30e18964c5b9c4c98ff7914b21fa08ae8
Johan-Romo <87491302+Johan-Romo@users.noreply.github.com>
2022-07-19 22:59:20 -0500

    Update ListaStr.h

--- Hash/TablaHashAbierta/ListaStr.h
+++ Hash/TablaHashAbierta/ListaStr.h
@@ -1,3 +1,10 @@
+/*
+Autores: Johan Romo, Cristhopher Villamarín.
+Fecha de creacion: 16/07/2022
+Fecha de modificacion: 19/07/2022
+Grupo 10
+Github del grupo: jaromo3@espe.edu.ec, cavillamarin9@espe.edu.ec
+*/
 #ifndef LISTASTR_H
 #define LISTASTR_H
  
