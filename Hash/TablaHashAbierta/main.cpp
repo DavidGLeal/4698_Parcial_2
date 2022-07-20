3e3eeb5ca8dd58a6eac2ac66ae28ff7dbbc70d15
Johan-Romo <87491302+Johan-Romo@users.noreply.github.com>
2022-07-19 22:55:02 -0500

    Update main.cpp

--- Hash/TablaHashAbierta/main.cpp
+++ Hash/TablaHashAbierta/main.cpp
@@ -1,9 +1,9 @@
 /*
-Autores: Shared Tinoco, Sebastian Torres.
+Autores: Johan Romo, Cristhopher Villamarín.
 Fecha de creacion: 26/06/2022
 Fecha de modificacion: 29/06/2022
 Grupo 10
-Github del grupo: mstinoco@espe.edu.ec, sptorres1@espe.edu.ec
+Github del grupo: jaromo3@espe.edu.ec, cavillamarin9@espe.edu.ec
 */
 
 #include "Menu.cpp"
