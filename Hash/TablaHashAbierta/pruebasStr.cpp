f0d4eff4c5b64e008cfc51dd9a67fcdcebc72c23
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2022-07-19 23:14:54 -0500

    Update pruebasStr.cpp

--- Hash/TablaHashAbierta/pruebasStr.cpp
+++ Hash/TablaHashAbierta/pruebasStr.cpp
@@ -1,3 +1,16 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+    *Enunciado del problema:
+	    *Programa que realize hash abierto.
+     *Autores:
+        *Curso de Estructura de Datos NRC: 4698
+	*Fecha de creacion: 
+        * 15-07-2022
+	*Fecha de modificacion:
+        * 19-07-2022
+    *GitHub del grupo:	
+        https://github.com/DavidGLeal/4698_Parcial_2/tree/Codigos_proyecto/Hash/TablaHashAbierta
+*/
 
 #include "HashStr.cpp"
 
