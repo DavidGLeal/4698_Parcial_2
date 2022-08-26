e3dae6e7cb1942661956a8ccb95e64b1a17c3858
LucadeVeintemilla <105328208+LucadeVeintemilla@users.noreply.github.com>
2022-08-26 00:18:25 -0500

    Add files via upload

---
+++ Extras/OrdenamientosInternos/Counting Sort/Sort.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#pragma once
+using namespace std;
+class Sort{
+	public:
+	void mostrar(int* , int );
+   
+	int obtenerMaximo(int* , int );
+   
+	void countSort(int* , int );
+};
+
+  
