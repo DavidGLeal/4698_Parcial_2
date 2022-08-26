0e33e780c1eca694ce6d247d3c48e288129eb495
Juan Francisco Reyes Salazar <105328338+JuanFReyes4@users.noreply.github.com>
2022-08-25 21:03:23 -0500

    Add files via upload

---
+++ Extras/ProyectosFinalesP3/Pacman/html/search/classes_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['imageentry_0',['ImageEntry',['../struct_image_entry.html',1,'']]],
+  ['imageinfo_1',['ImageInfo',['../struct_image_info.html',1,'']]],
+  ['ingresardatos_2',['IngresarDatos',['../class_ingresar_datos.html',1,'']]]
+];
---
+++ Extras/ProyectosFinalesP3/Pacman/html/search/classes_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['menu_0',['MENU',['../class_m_e_n_u.html',1,'']]],
+  ['metrics_1',['Metrics',['../class_metrics.html',1,'']]]
+];
