3d51f8d77db31b5e80dfa9cc050a8f35307d72fa
Jhon Munarco Santos <105329243+jhonsantos94@users.noreply.github.com>
2022-08-31 18:54:31 -0500

    Add files via upload

---
+++ CODIGOS MANUAL/CODIGOS CAP 10/Evaluacion_Polinomio/Evaluacion Polinomio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Metodos.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+double evaluaPolinomio1(double[], double );
+
+
+int main(int argc, char** argv) {
+	
+	
+	Metodos obj1;
+	
+	
+	return 0;
+}
+
